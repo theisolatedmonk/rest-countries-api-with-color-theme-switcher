23e493465c0ea95b8d4e879f93c6acead448012c
theisolatedmonk <96rahulseth@gmail.com>
2023-05-09 23:48:23 +0530

    initial commit

---
+++ src/app/SelectLabels.tsx
@@ -0,0 +1,36 @@
+"use client"
+import * as React from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+export default function SelectLabels() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <div>
+     
+      <FormControl sx={{ m: 1, minWidth: 180, }}>
+        <Select sx={{ m: 1, height:40 }}
+          value={age}
+          onChange={handleChange}
+          displayEmpty
+          inputProps={{ 'aria-label': 'Without label' }}
+        >
+          <MenuItem value="">
+            <em>Filter by Region</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+        <FormHelperText></FormHelperText>
+      </FormControl>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import darkMode from '@/images/moon-outline.svg'
 import search from '@/images/search-outline.svg'
+import SelectLabels from './SelectLabels'
 
 export default function Home() {
   return (
@@ -12,12 +13,53 @@ export default function Home() {
           <p>Dark Mode</p>
         </div>
       </div>
-      <div className="flex justify-between w-40 shadow-lg">
-          <button className='border-none h-6 w-6 bg-black'>
-         
-          </button>
-         <input type="text" />
+      <hr className='h-full' />
+<div className="flex flex-col w-full ">
+<div className="flex justify-between items-center w-full ">
+     <div className="  w-96  shadow-lg relative p-1 h-10  rounded-md ">
+          <Image  className='h-5 w-5 top-3 left-4 bg-transparent absolute z-10' src={search} alt={''}/>
+         <input type="text" className='w-full h-full top-0 left-0 absolute rounded-md' />
+        </div> 
+       <div className=" "> <SelectLabels/></div>
+     </div>
+     <div className="flex flex-col h-64 w-52 rounded-md  bg-gray-600 overflow-hidden ">
+      <div className=" h-32 w-full bg-black"></div>
+      <div className="flex flex-col p-4">
+        <p className="text-sm">Germany</p>
+        <div className="text-sm ">
+          <p>Population: 81,770,900</p>
+          <p>Region: Europe </p>
+          <p>Capital: Berlin</p>
         </div>
+      </div>
+
+     </div>
+</div>
+
+<div className="flex flex-col hidden">
+  <button className='h-4 w-8 rounded-lg'>Back</button>
+  <div className="flex">
+    <div className="h-64 w-80"></div>
+    <div className="flex flex-col">
+      <div className="flex">Belgium</div>
+      <div className="flex">
+        <div className="">
+          <span>Native Name</span><span>Native Name</span>
+        </div>
+        <div className="flex flex-col"></div>
+      </div>
+      <div className="flex">
+        <p className="">Border Countries:</p>
+        <div className="">
+        <p className=""></p>
+        <p className=""></p>
+        <p className=""></p>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</div>
     </main>
   )
 }
