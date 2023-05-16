91bd0f1636ba0142a55ba78b4984fcc0af1ffba0
theisolatedmonk <96rahulseth@gmail.com>
2023-05-17 00:25:44 +0530

    tag line is made link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,17 @@
 
 import { useTheme } from "next-themes";
 import ToggalButton from "./ToggalButton";
+import Link from "next/link";
 
 export function Header() {
-  return (
+  return ( <Link
+    href={"/"}
+  >
+ 
     <div className="flex  justify-between w-full py-4 px-12  shadow-lg items-center   z-50 rounded-sm dark:bg-[hsl(209,23%,22%)] dark:text-[hsl(0,0%,100%)] top-0 sticky bg-white">
       <p className="text-lg font-bold">Where in the world?</p>
       <ToggalButton />
     </div>
+    </Link>
   );
 }
